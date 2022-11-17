f79be912ebee26aa291a68ca4d863eadfd34640d
Felipe <felipe-daros@hotmail.com>
2022-11-17 16:53:08 -0300

    .

--- App.tsx
+++ App.tsx
@@ -3,11 +3,18 @@ import { ThemeProvider } from 'styled-components';
 import theme from '@theme/index';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import Loading from '@components/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+  
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
